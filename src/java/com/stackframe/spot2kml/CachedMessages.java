f125b5c28cfaf44562d1041fa1ea243110e58ddf
Gene McCulley <mcculley@stackframe.com>
2011-10-16 15:26:58 -0400

    Documentation.

--- src/java/com/stackframe/spot2kml/CachedMessages.java
+++ src/java/com/stackframe/spot2kml/CachedMessages.java
@@ -15,6 +15,7 @@ import java.util.TreeSet;
  */
 public class CachedMessages {
 
+    // FIXME: Need to cap the maximum number of messages kept.
     private long lastRetrieve;
     private long lastUpdate;
     private static final Comparator<SPOTMessage> messageTimeComparator = new Comparator<SPOTMessage>() {
@@ -25,11 +26,21 @@ public class CachedMessages {
     };
     private final SortedSet<SPOTMessage> messages = new TreeSet<SPOTMessage>(messageTimeComparator);
 
+    /**
+     * Add a Collection of SPOTMessage objects to this cache.
+     *
+     * @param m a Collection of SPOTMessage objects
+     */
     public synchronized void addAll(Collection<SPOTMessage> m) {
         messages.addAll(m);
         lastUpdate = System.currentTimeMillis();
     }
 
+    /**
+     * Returns a SortedSet of SPOTMessage objects. The messages are sorted such that the latest message is first and the oldest is last.
+     *
+     * @return the sorted messages
+     */
     public synchronized SortedSet<SPOTMessage> getMessages() {
         SortedSet<SPOTMessage> copy = new TreeSet<SPOTMessage>(messageTimeComparator);
         copy.addAll(messages);
@@ -37,10 +48,20 @@ public class CachedMessages {
         return copy;
     }
 
+    /**
+     * Gets the last time the messages were retrieved, in milliseconds since midnight, January 1, 1970 UTC.
+     *
+     * @return the last time the messages were retrieved
+     */
     public long getLastRetrieve() {
         return lastRetrieve;
     }
 
+    /**
+     * Gets the last time the messages were updated, in milliseconds since midnight, January 1, 1970 UTC.
+     *
+     * @return the last time the messages were updated
+     */
     public long getLastUpdate() {
         return lastUpdate;
     }
